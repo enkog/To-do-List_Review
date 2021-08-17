dfe410cc3ea1c73f8f4af5b902ea5bfff95b2d6b
enkog <nkirukaogu@gmail.com>
2021-08-17 16:38:09 +0100

    Set an array of some simple to do tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import _ from 'lodash';
 import './style.css';
+
+const todoArr = [
+  { description: 'Get groceries', completed: false, index: 1 },
+  { description: 'Wash the dishes', completed: false, index: 2 },
+  { description: 'Complete todo list project', completed: false, index: 3 },
+];
