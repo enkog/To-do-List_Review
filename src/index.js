63712bf6a00b1f2490d6d974578bbb776e47c6fa
enkog <nkirukaogu@gmail.com>
2021-08-18 10:32:01 +0100

    Store the updated array of items in local storage

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const todoArr = [
   { description: 'Complete todo list project', completed: false, index: 3 },
 ];
 
-const displayTodo = (arr) => {
+const displayTodo = (arr, actions) => {
   const todoListContainer = document.querySelector('.todo-list');
   const ul = document.createElement('ul');
   todoListContainer.appendChild(ul);
@@ -27,7 +27,7 @@ const displayTodo = (arr) => {
     const listMenuIcon = document.createElement('i');
     listMenuIcon.className = 'fas fa-ellipsis-v';
 
-    const tasks = { li, arr };
+    const tasks = { li, arr, actions };
     checkBox.addEventListener('change', taskComplete.bind(null, tasks));
 
     li.appendChild(checkBox);
@@ -37,4 +37,12 @@ const displayTodo = (arr) => {
   });
 };
 
-displayTodo(todoArr);
+const actions = new LocalStorageActions();
+const localTodos = actions.get();
+
+if (localTodos.length === 0) {
+  actions.add(todoArr);
+  displayTodo(todoArr, actions);
+} else {
+  displayTodo(localTodos, actions);
+}
